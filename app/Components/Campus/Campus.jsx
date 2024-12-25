b27a8b1bf2e54d175be6c9d8ba1cd526004b4786
karim selim <kareemsleem7@gmail.com>
2024-12-25 08:26:50 +0200

    changing the project

---
+++ app/Components/Campus/Campus.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line
+import React from "react";
+import "./Campus.css";
+import gallery_1 from "../../assets/gallery-1.png";
+import gallery_2 from "../../assets/gallery-2.png";
+import gallery_3 from "../../assets/gallery-3.png";
+import gallery_4 from "../../assets/gallery-4.png";
+import white_arrow from "../../assets/white-arrow.png";
+import Image from "next/image";
+
+const Campus = () => {
+  return (
+    <div className="campus">
+      <div className="gallery">
+        <Image src={gallery_1} alt="" />
+        <Image src={gallery_2} alt="" />
+        <Image src={gallery_3} alt="" />
+        <Image src={gallery_4} alt="" />
+      </div>
+      <button className="btn dark-btn">
+        See more here <Image src={white_arrow} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Campus;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|avi)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
