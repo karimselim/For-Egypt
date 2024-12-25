3a5fa8871ecce24162466d2c4bd280fdb910e7b6
karim selim <kareemsleem7@gmail.com>
2024-12-26 01:07:06 +0200

    main section

--- app/Components/Hero/Hero.jsx
+++ app/Components/Hero/Hero.jsx
@@ -6,19 +6,15 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="hero ">
+    <main className="hero">
       <div className="hero-text">
-        <h1>We Ensure better education for a better world</h1>
-        <p>
-          Our cutting-edge curriculum is designed to empower students with the
-          knowledge, skills, and experience nedded to excel in the dynamic field
-          of education
-        </p>
+        <h1>أسرة طلاب من أجل مصر</h1>
+        <p>طلاب من أجل مصر - معًا مهمتنا بالقوة والشرف</p>
         <button className="btn">
           Explore more <Image src={dark_arrow} alt=""></Image>
         </button>
       </div>
-    </div>
+    </main>
   );
 };
 
