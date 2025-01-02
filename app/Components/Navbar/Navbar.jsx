719142ab0616f2459f4071b55f000ea8245932b9
karim selim <kareemsleem7@gmail.com>
2025-01-02 15:36:17 +0200

    navbar: making it more arabic and put a close button in the menu also adding the uni name

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { CiMenuFries } from "react-icons/ci";
 import Image from "next/image";
 import forEgypt from "../../assets/forEgyptlogo.png";
 import uni from "../../assets/uniLogo.png";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const sideBarRef = useRef(null);
@@ -19,10 +20,8 @@ const Navbar = () => {
 
   const handleClickOutside = (event) => {
     if (
-      sideBarRef.current &&
-      !sideBarRef.current.contains(event.target) &&
-      menuIconRef.current &&
-      !menuIconRef.current.contains(event.target)
+      (sideBarRef.current && !sideBarRef.current.contains(event.target)) ||
+      (menuIconRef.current && menuIconRef.current.contains(event.target))
     ) {
       setMobileMenu(false);
     }
@@ -54,54 +53,42 @@ const Navbar = () => {
         sticky ? "bg-main py-4" : "py-1"
       }`}
     >
-      <Link smooth={true} to="hero" offset={0} duration={500}>
-        <h2 className="text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-2xl flex gap-2 max-sm:text-2xl z-20">
-          <Image
-            src={forEgypt}
-            alt=""
-            className="w-8 max-sm:w-6 object-contain"
-          />
-          <Image src={uni} alt="" className="w-8 max-sm:w-6 object-contain" />
-          طلاب من أجل مصر
-        </h2>
-      </Link>
+      <CiMenuFries
+        className="text-3xl hidden max-md:block z-10"
+        onClick={toggleMenu}
+      />
       <ul
-        className={`z-30 max-md:fixed max-md:top-0 max-md:bottom-0 max-md:bg-main max-md:w-[65%] max-md:pt-[70px] max-md:duration-300 ${
-          mobileMenu ? " max-md:right-0" : "-right-[65%]"
+        className={`z-30 max-md:fixed max-md:flex-col-reverse max-md:justify-end max-md:top-0 max-md:bottom-0 max-md:text-right max-md:bg-main max-md:w-[65%] max-md:pt-[70px] max-md:duration-300 ${
+          mobileMenu ? " max-md:left-0 max-md:flex" : "-left-[65%]"
         }`}
         ref={sideBarRef}
       >
-        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1 max-xl:hidden">
-          <Link
-            className="w-full block"
-            to="hero"
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            الصفحة الرئيسية
-          </Link>
+        <IoMdClose
+          className="md:hidden absolute top-4 right-10 text-3xl cursor-pointer"
+          ref={menuIconRef}
+        />
+        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
+          <Button className="bg-white text-black max-xl:px-4 max-md:hidden">
+            <Link
+              className="w-full block"
+              to="contact"
+              smooth={true}
+              offset={-260}
+              duration={500}
+            >
+              انضم الينا الآن
+            </Link>
+          </Button>
         </li>
         <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
           <Link
             className="w-full block"
-            to="program"
+            to="testimonials"
             smooth={true}
             offset={-260}
             duration={500}
           >
-            خدماتنا
-          </Link>
-        </li>
-        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
-          <Link
-            className="w-full block"
-            to="about"
-            smooth={true}
-            offset={-150}
-            duration={500}
-          >
-            من نحن؟
+            آراء طلابنا
           </Link>
         </li>
         <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
@@ -118,33 +105,51 @@ const Navbar = () => {
         <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
           <Link
             className="w-full block"
-            to="testimonials"
+            to="about"
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            من نحن؟
+          </Link>
+        </li>
+        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
+          <Link
+            className="w-full block"
+            to="program"
             smooth={true}
             offset={-260}
             duration={500}
           >
-            آراء طلابنا
+            خدماتنا
           </Link>
         </li>
-        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1">
-          <Button className="bg-white text-black max-xl:px-4">
-            <Link
-              className="w-full block"
-              to="contact"
-              smooth={true}
-              offset={-260}
-              duration={500}
-            >
-              انضم الينا الآن
-            </Link>
-          </Button>
+        <li className="text-xl max-lg:text-base max-md:text-xl hover:text-black transition-colors max-lg:my-[10px] max-lg:mx-3 max-md:block max-md:my-6 max-md:mx-10 duration-300 inline-block mx-5 my-1 max-xl:hidden">
+          <Link
+            className="w-full block"
+            to="hero"
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            الصفحة الرئيسية
+          </Link>
         </li>
       </ul>
-      <CiMenuFries
-        className="text-3xl hidden max-md:block z-50"
-        onClick={toggleMenu}
-        ref={menuIconRef}
-      />
+      <Link smooth={true} to="hero" offset={0} duration={500}>
+        <div className="text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-2xl flex gap-2 max-sm:text-2xl z-20">
+          <Image
+            src={forEgypt}
+            alt=""
+            className="w-12 max-sm:w-6 object-contain"
+          />
+          <Image src={uni} alt="" className="w-12 max-sm:w-6 object-contain" />
+          <div>
+            <h2> طلاب من أجل مصر</h2>
+            <span className="text-base text-right block">جامعة بورسعيد</span>
+          </div>
+        </div>
+      </Link>
     </nav>
   );
 };
