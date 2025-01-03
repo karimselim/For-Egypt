49798a79764ba4f193d8e38e43115f1f4411e1b9
karim selim <kareemsleem7@gmail.com>
2025-01-03 12:53:19 +0200

    title and footer: adding tailwind to title and removing the title and footer css files

--- app/Components/Title/Title.jsx
+++ app/Components/Title/Title.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
 import React from "react";
-import "./Title.css";
+// import "./Title.css";
 
 const Title = ({ subTitle, title }) => {
   return (
-    <div className="title">
+    <div className="title text-center text-main font-semibold uppercase mt-[70px] mb-[30px]">
       <p className="text-3xl">{subTitle}</p>
-      <h2 className="text-4xl">{title}</h2>
+      <h2 className="text-4xl text-[#000f38] mt-1">{title}</h2>
     </div>
   );
 };
