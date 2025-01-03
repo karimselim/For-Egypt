8bada6b589223b3f1592a31ceb2dd2ecc230bf4c
karim selim <kareemsleem7@gmail.com>
2025-01-03 11:58:56 +0200

    Programms: adding some addittionally rendered placeholder content for the Programs

--- app/Components/Programs/Programs.jsx
+++ app/Components/Programs/Programs.jsx
@@ -18,7 +18,7 @@ const useIsMobile = () => {
         typeof navigator === "undefined" ? "" : navigator.userAgent;
       const isPhone =
         /iPhone|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-      setIsMobile(isPhone || window.innerWidth <= 992);
+      setIsMobile(isPhone || window.innerWidth <= 768); // Consider smaller screens as mobile
     };
 
     checkMobile();
@@ -34,63 +34,51 @@ const useIsMobile = () => {
 
 const Programs = () => {
   const isMobile = useIsMobile(); // Check if the device is mobile
+  console.log(isMobile);
 
   return (
-    <section
-      className={`programs mx-[7%] my-20 flex ${
-        isMobile ? "flex-col gap-6" : "justify-between gap-12"
-      }`}
-    >
-      <div
-        className={`program flex-basis-[31%] relative ${
-          isMobile ? "w-full" : ""
-        }`}
-      >
-        <Image src={program_1} alt="" className="w-full block rounded-[10px]" />
-        <div
-          className={`caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 ${
-            isMobile
-              ? "bg-[rgba(152,0,25,0.6)] flex items-center justify-center text-white pt-0 opacity-100"
-              : "bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0"
-          }`}
-        >
-          <FaBookReader className="text-6xl mb-2" />
-          <p>كورسات و تدريبات</p>
+    <section className="programs mx-[7%] my-20 flex justify-between max-md:flex-col gap-12">
+      <div className="relative">
+        <div className="program flex-basis-[31%] relative">
+          <Image
+            src={program_1}
+            alt=""
+            className="w-full block rounded-[10px]"
+          />
+          <div className="caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0">
+            <FaBookReader className="text-6xl mb-2" />
+            <p>كورسات و تدريبات</p>
+          </div>
         </div>
+        {isMobile && <p>this is the content for the courses</p>}
       </div>
-      <div
-        className={`program flex-basis-[31%] relative ${
-          isMobile ? "w-full" : ""
-        }`}
-      >
-        <Image src={program_2} alt="" className="w-full block rounded-[10px]" />
-        <div
-          className={`caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 ${
-            isMobile
-              ? "bg-[rgba(152,0,25,0.6)] flex items-center justify-center text-white pt-0 opacity-100"
-              : "bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0"
-          }`}
-        >
-          <CiDiscount1 className="text-6xl mb-2" />
-          <p>خصومات حصرية</p>
+      <div className="relative">
+        <div className="program flex-basis-[31%] relative">
+          <Image
+            src={program_2}
+            alt=""
+            className="w-full block rounded-[10px]"
+          />
+          <div className="caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0">
+            <CiDiscount1 className="text-6xl mb-2" />
+            <p>خصومات حصرية</p>
+          </div>
         </div>
+        {isMobile && <p>this is the contnet of discounts</p>}
       </div>
-      <div
-        className={`program flex-basis-[31%] relative ${
-          isMobile ? "w-full" : ""
-        }`}
-      >
-        <Image src={program_3} alt="" className="w-full block rounded-[10px]" />
-        <div
-          className={`caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 ${
-            isMobile
-              ? "bg-[rgba(152,0,25,0.6)] flex items-center justify-center text-white pt-0 opacity-100"
-              : "bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0"
-          }`}
-        >
-          <CiCalendarDate className="text-6xl mb-2" />
-          <p>فعاليات متنوعة</p>
+      <div className="relative">
+        <div className="program flex-basis-[31%] relative">
+          <Image
+            src={program_3}
+            alt=""
+            className="w-full block rounded-[10px]"
+          />
+          <div className="caption rounded-[10px] absolute top-0 left-0 right-0 bottom-0 bg-[rgba(152,0,25,0.3)] flex items-center justify-center flex-col text-white cursor-pointer opacity-0 pt-[70%] transition-all duration-400 hover:opacity-100 hover:pt-0">
+            <CiCalendarDate className="text-6xl mb-2" />
+            <p>فعاليات متنوعة</p>
+          </div>
         </div>
+        {isMobile && <p>this is the content of events</p>}
       </div>
     </section>
   );
