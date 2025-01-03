df89d02d459a67059ca69dfd891defb1654874c4
karim selim <kareemsleem7@gmail.com>
2025-01-03 12:55:00 +0200

    making footer elements cursor pointer

--- app/Components/Footer/Footer.jsx
+++ app/Components/Footer/Footer.jsx
@@ -6,8 +6,12 @@ const Footer = () => {
     <footer className="footer px-[7%] py-4 flex items-center justify-between border-t border-t-[#797979] max-md:block max-md:text-center">
       <p>© جميع الحقوق مستحقة لأسرة طلاب من أجل مصر </p>
       <ul className="mt-[10px]">
-        <li className="inline-block ml-5 max-md:m-[10px]">شروط الاستخدام</li>
-        <li className="inline-block ml-5 max-md:m-[10px]">سياسة الخصوصية</li>
+        <li className="inline-block ml-5 max-md:m-[10px] cursor-pointer">
+          شروط الاستخدام
+        </li>
+        <li className="inline-block ml-5 max-md:m-[10px] cursor-pointer">
+          سياسة الخصوصية
+        </li>
       </ul>
     </footer>
   );
