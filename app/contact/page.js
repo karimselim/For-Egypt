1cf504f2b70d348b3e4eec86a5f7c0341af3c111
karim selim <kareemsleem7@gmail.com>
2025-01-28 05:45:23 +0200

    starting contact us page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>contact us</div>;
+  return (
+    <main className="bg-slate-100 h-screen w-screen">
+      <div className="w-screen h-2/3 bg-main rounded-b-xl shadow-xl top-[72px] relative">
+        <aside></aside>
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
