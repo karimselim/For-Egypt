4d34550e7c7b64d465dc434b8c91929a876637de
karim selim <kareemsleem7@gmail.com>
2025-02-09 14:06:37 +0200

    making the contact page responsive

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,9 +5,9 @@ import Button from "../UI/Button";
 const Contact = () => {
   return (
     <main className="bg-slate-100 h-screen w-screen overflow-hidden">
-      <div className="w-screen px-[7%] flex gap-32 h-[calc(67%-88px)] max-lg:h-[calc(67%-100px)] max-md:max-md:h-[calc(67%-80px)] bg-main rounded-b-xl shadow-xl top-[88px] max-md:top-[80px] max-lg:top-[100px] relative">
-        <aside className="w-[calc(50%-50px)] text-white">
-          <h2 className="text-xl pt-16 text-center">الشكاوى و المقترحات</h2>
+      <div className="w-screen px-[7%] flex max-md:flex-col gap-32 h-[calc(67%-88px)] max-lg:h-[calc(67%-100px)] max-md:max-md:h-[calc(67%-80px)] bg-main rounded-b-xl shadow-xl top-[88px] max-md:top-[80px] max-lg:top-[100px] relative">
+        <aside className="w-[calc(50%-50px)] text-white max-md:hidden">
+          <h2 className="text-xl pt-6 text-center">الشكاوى و المقترحات</h2>
           <div className="flex flex-row-reverse justify-between items-center my-5 max-md:block">
             <p>الإيميل</p>
             <Link
@@ -26,27 +26,36 @@ const Contact = () => {
             <p>بورسعيد, بورفؤاد</p>
           </div>
         </aside>
-        <form className="flex flex-col w-1/2 mt-16 bg-white items-end py-8 px-12 h-fit rounded-md">
+        <form className="flex flex-col w-1/2 mt-6 bg-white items-end py-8 px-12 h-fit rounded-md max-md:w-full">
           <label className="block w-full text-right">الإسم</label>
           <input
-            placeholder="أدخل إسمك الكامل"
+            placeholder="إسمك الكامل"
             className="block w-full border p-3 outline-none mb-2 mt-1 text-right rounded-md"
           />
-          <label className="block w-full text-right">البريد الإلكتروني</label>
+          <label className="block w-full text-right">الرقم القومي</label>
           <input
-            placeholder="أدخل بريدك الإلكتروني"
+            placeholder="الرقم القومي"
             className="block w-full border p-3 outline-none mb-2 mt-1 text-right rounded-md"
           />
           <label className="block w-full text-right">
             ( whatsApp يدعم ) رقم الهاتف
           </label>
           <input
-            placeholder="أدخل رقم الهاتف"
+            placeholder="رقم الهاتف"
+            className="block w-full border p-3 outline-none mb-2 mt-1 text-right rounded-md"
+          />
+          <label className="block w-full text-right">الكلية</label>
+          <input
+            placeholder="إسم الكلية"
             className="block w-full border p-3 outline-none mb-2 mt-1 text-right rounded-md"
           />
           <label className="block w-full text-right">الرسالة</label>
-          <textarea
+          {/* <textarea
             className="border w-full h-28 outline-none p-3 text-right mt-2"
+            placeholder="... شاركنا ملاحظاتك او إقتراحاتك "
+          /> */}
+          <input
+            className="border w-full outline-none p-3 text-right mt-2"
             placeholder="... شاركنا ملاحظاتك او إقتراحاتك "
           />
           <Button className="p-4 mt-4">إرسال</Button>
@@ -60,7 +69,7 @@ const Contact = () => {
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
-        className="pl-[7%] absolute bottom-4 rounded-xl"
+        className="pl-[7%] absolute bottom-4 rounded-xl max-md:hidden"
         // aria-label="port said university"
       ></iframe>
     </main>
