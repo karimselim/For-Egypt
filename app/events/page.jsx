718492af3a2aa9a459cb0c34429b76583137b664
karim selim <kareemsleem7@gmail.com>
2025-02-10 16:59:19 +0200

    making the height fit for the text

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -6,7 +6,7 @@ const Events = () => {
   return (
     <main className="w-screen h-screen overflow-hidden">
       <div className="w-screen px-[7%] flex max-md:flex-col gap-32 h-[calc(67%-88px)] max-lg:h-[calc(67%-100px)] max-md:max-md:h-[calc(67%-80px)] bg-main rounded-b-xl shadow-xl top-[88px] max-md:top-[80px] max-lg:top-[100px] relative min-h-fit pb-6">
-        <div className="flex w-full gap-6">
+        <div className="flex w-full gap-6 min-h-fit">
           <div className="w-1/2 h-4/5 pt-2">
             <Image
               src={img}
@@ -14,7 +14,7 @@ const Events = () => {
               className="object-cover rounded-xl h-full"
             />
           </div>
-          <div className="pt-12 w-1/2 text-white">
+          <div className="pt-12 w-1/2 text-white min-h-fit">
             <h1 className="text-2xl">this is the event</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
